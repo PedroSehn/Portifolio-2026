bc09dac8119752ff9850cc56610620649e939898
Pedro <pedrorsehn@hotmail.com>
2026-03-03 17:44:17 -0300

    Update Desktop component layout to set maximum width to 1200px for better responsiveness

--- src/components/layout/Desktop.tsx
+++ src/components/layout/Desktop.tsx
@@ -3,7 +3,7 @@ import type { PropsWithChildren } from 'react'
 export default function Desktop({ children }: PropsWithChildren<{}>) {
   return (
     <main className="min-h-screen bg-win-teal font-win px-[24px] pt-[20px] pr-[24px] pb-[50px] pl-[24px] md:pl-[100px] flex flex-col gap-5">
-      <div className="w-full md:max-w-6xl md:mx-auto flex flex-col gap-5">
+      <div className="w-full md:max-w-[1200px] md:mx-auto flex flex-col gap-5">
         {children}
       </div>
     </main>
